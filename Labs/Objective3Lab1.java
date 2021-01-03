93a983d1ce121b43a1f1a8b8b58487dc0b5dcc63
ramon <ramonarmen96@gmail.com>
2021-01-03 16:08:43 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main (String[] args) {
+double num1, num2, num3, average;
+     num1 = 6;
+     num2 = 2;
+     num3 = 15;
+    average = (num1+num2+num3)/3;
+    System.out.print(average);
+  }
+}
