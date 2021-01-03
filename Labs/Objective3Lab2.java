f51a1865892829e0513a800dd8296cbadc9948b1
ramon <ramonarmen96@gmail.com>
2021-01-03 16:26:18 -0500

    complete objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,27 @@
+public class Objective3Lab2 {
+public static void main(String[] args) {
+  double num1, num2, num3, num4, num5, num6;
+
+  num1 = 10.0;
+
+  num2 = 8.0;
+
+  num3 = (10*10);
+
+  num4 = (8*8);
+
+  num5 = Math.sqrt (num3 + num4);
+
+  System.out.print("The hypotenuse of a triangle with sides ");
+
+  System.out.print(num1);
+
+  System.out.print(" and ");
+
+  System.out.print(num2);
+
+  System.out.print(" is ");
+
+  System.out.print(num5);
+}
+}
