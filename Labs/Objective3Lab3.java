d8f8a299ca06cff1656d1be313c8f1fa7ec97579
ramon <ramonarmen96@gmail.com>
2021-01-03 16:40:34 -0500

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,16 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int first = 1 + (int)(Math.random() *6);
+    int second = 1 + (int)(Math.random() *6);
+    int third = 1 + (int)(Math.random() *6);
+    int total = first + second + third ;
+
+    System.out.print(first);
+    System.out.print(" + ");
+    System.out.print(second);
+    System.out.print(" + ");
+    System.out.print(third);
+    System.out.print(" = ");
+    System.out.print(total);
+  }
+}
