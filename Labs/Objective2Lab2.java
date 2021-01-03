9e9cb3ae8404d0fd59bc853fae1374e89e1e308a
ramon <ramonarmen96@gmail.com>
2021-01-03 02:43:45 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main (String[] args) {
+    String name = "Ramon Armendariz ";
+    int age = 24 ;
+    String state = "Texas ";
+    System.out.print("Name: " );
+    System.out.print(name );
+    System.out.print("Age: ");
+    System.out.print(age );
+    System.out.print(" State: ");
+    System.out.print(state);
+  }
+}
