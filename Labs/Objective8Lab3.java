34447112843b789a87d14cb24936802c4e49794b
ramon <ramonarmen96@gmail.com>
2021-01-04 14:43:55 -0500

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,16 @@
+public class Objective8Lab3{
+  public static void main(String[] args){
+
+int counter = 1;
+
+for  (int i=1;  i<= 20; i += 1)
+if (i % 2 ==0){
+
+  System.out.println(i+ " is even");
+}
+else{
+  System.out.println(i+ " is odd");
+
+    }
+  }
+}
